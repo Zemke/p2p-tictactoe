100defe1e2bb8444584829a867b3bb34c0f5321a
Zemke <florian.zemke@ewetel.net>
2014-04-03 16:08:41 +0200

    Now using isaacs/node-supervisor and jprichardson/reload (both on GitHub) for automatic server restart and browser refresh. Awesome tools.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var reload = require('reload');
 
 var app = express();
 
@@ -31,6 +32,10 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var server = http.createServer(app);
+
+reload(server, app); // https://github.com/jprichardson/reload
+
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
