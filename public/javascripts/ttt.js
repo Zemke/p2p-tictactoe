d1871b784ce145d768c6c5a8c399936792c47cdb
Zemke <florian.zemke@ewetel.net>
2014-04-03 16:09:07 +0200

    Peers can now connect and test data is sent.

--- public/javascripts/ttt.js
+++ public/javascripts/ttt.js
@@ -4,11 +4,26 @@ tttApp.controller('TttAppCtrl', function ($scope) {
     var peersId = randomString();
     var peer = new Peer(peersId, {key: '4o5t8qjc9ncrqkt9'});
 
-    peer.on('open', function (id) {
-        console.log('open event emitted');
-        console.log(id);
+    peer.on('connection', function (conn) {
+        conn.on('data', function (data) {
+            console.log(data);
+        });
     });
 
+    peer.on('error', function (err) {
+        console.error('PeerJS error event emitted:');
+        console.error(err.type);
+        console.error("http://peerjs.com/docs/#peeron-error");
+    });
+
+    $scope.startGame = function () {
+        var conn = peer.connect($scope.otherPeersId);
+        conn.on('open', function () {
+            conn.send('Hey, other peer!');
+        });
+    };
+
+    $scope.otherPeersId = "";
     $scope.peersId = peersId;
 });
 
